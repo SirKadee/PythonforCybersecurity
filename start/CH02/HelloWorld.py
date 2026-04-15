1edee16e9c6ada926fc30fac34eb490cc5463116
TueVan <Van.Tue@student.greenriver.edu>
2026-04-15 11:29:24 -0700

    Hello World Expanded

--- start/CH02/HelloWorld.py
+++ start/CH02/HelloWorld.py
@@ -5,5 +5,24 @@
 # ask user for their name
 user_name = input("What is your name: ")
 
-# Write Hello World to the user to the screen
+# Write Hello using concatenation
 print("Hello " + user_name)
+
+# string formatting and f-string
+print("Hello {0}".format(user_name))
+print(f"Hello {user_name}")
+
+# multiple print statements
+print("Hello", user_name)
+print("Hello ", end="")
+print(user_name)
+
+# Create new variables
+message = "Hello " + user_name
+print(message)
+
+# join text
+print( " ".join(["Hello", user_name]))
+
+# old style formatting
+print("Hello %s" % user_name)
