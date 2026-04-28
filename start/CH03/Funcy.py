b56e1348cbeaeaef1b5bafefe9103214b613afbd
TueVan <Van.Tue@student.greenriver.edu>
2026-04-28 15:09:23 -0700

    Funcy Completed

--- start/CH03/Funcy.py
+++ start/CH03/Funcy.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
 # example workign with Functions
-#By 
+#By Devin Van
+
+#Create The Function
+def print_me(message):
+  print(message)
+  return("returned value")
+
+#Call The Function
+print("Before Function")
+
+error_message = "This is an error"
+returned_value = print_me(error_message)
+print(returned_value)
+
+print("After Function")
