2c79bc9d0adc779e51178f3b5cc5287712edabfb
TueVan <Van.Tue@student.greenriver.edu>
2026-04-28 20:23:08 -0700

    Completed Writefile

--- start/CH04/WriteFile.py
+++ start/CH04/WriteFile.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python3
 # Sample script that writes to a file
-# By 
+# By Devin Van
+
+# open file for writing
+f = open("testfile.txt", "w")
+
+# Write some content to file
+print("Hello World")
+f.write("Hello World\n")
+f.write("Hello World\n")
+f.write("Hello World\n")
+
+# Close the file
+f.close()
